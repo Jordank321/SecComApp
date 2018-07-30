ef3cbdf0fad0612b7fb762974444d6de63b47f6a
Jordan Kelwick <jordankelwick@gmail.com>
2018-07-30 08:30:20 +0100

    WIP

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
   Text,
   View
 } from 'react-native';
+import Main from './components/Main';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,20 +20,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Main />
       </View>
     );
   }
